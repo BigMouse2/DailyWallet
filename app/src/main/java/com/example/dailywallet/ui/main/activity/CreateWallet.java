cf0a241c61f498a5440841e211bb8e8d95f58d9f
BigMouse2 <48596915+BigMouse2@users.noreply.github.com>
2021-11-05 16:46:40 -0400

    First Data in Firestore

--- app/src/main/java/com/example/dailywallet/ui/main/activity/CreateWallet.java
+++ app/src/main/java/com/example/dailywallet/ui/main/activity/CreateWallet.java
@@ -1,27 +1,52 @@
 package com.example.dailywallet.ui.main.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.dailywallet.MainActivity;
 import com.example.dailywallet.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateWallet extends AppCompatActivity {
+
+    private static final String TAG = "CreateWallet";
+
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_wallet);
 
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+
         Button button = (Button) findViewById(R.id.save);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivityMainActivity();
+             //   openActivityMainActivity();
+                System.out.println("changement de page");
+                saveNote();
             }
         });
     }
@@ -29,6 +54,23 @@ public class CreateWallet extends AppCompatActivity {
     public void openActivityMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void saveNote(){
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+
+        Map<String, Object> note = new HashMap<>();
+
+        note.put(KEY_TITLE,title);
+        note.put(KEY_DESCRIPTION,description);
+
+        db.collection("Notebook").document("My First Note").set(note)
+                .addOnSuccessListener(aVoid -> Toast.makeText(CreateWallet.this, "Note Saved", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> {
+                    Toast.makeText(CreateWallet.this, "Error!", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, e.toString());
+                });
     }
 
 }
